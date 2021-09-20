556fb62575fa2657302000fd03bf7c24bc22b02b
Anderson <laventurepsu1@gmail.com>
2021-09-20 16:23:43 -0400

    refracter:error messages are displayed on sign up

--- client/src/UserMessages.js
+++ client/src/UserMessages.js
@@ -1,25 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 const UserMessages = ({ showUserMsgs }) => {
+
+    const userMsgs = showUserMsgs.map((msg, index) => ( <li key={index}>{msg}</li>));
+
+
     const showMsgThenRemove = (userMsgs) => {
-
-
         setTimeout(function () {
-           userMsgs.remove();
-          },3000)
+            userMsgs.remove();
+        }, 3000);
+    };
 
-    }
 
+    console.log(showUserMsgs.length );
 
-    const userMsgs = showUserMsgs.map((msg, index) => (
-        <li key={index}>{msg}</li>
-    ));
-
-    return (
-        <>
-            <ul>{showMsgThenRemove(userMsgs)}</ul>
-        </>
-    );
+    return <>{showUserMsgs.length > 0 ? <ul>{userMsgs}</ul> : ''}</>;
 };
 
 export default UserMessages;

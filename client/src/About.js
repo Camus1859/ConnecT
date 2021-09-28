e1f9196a193bdd2b21e532085be3958fb36955dd
Anderson <laventurepsu1@gmail.com>
2021-09-28 19:54:05 -0400

    chore: added about page info

--- client/src/About.js
+++ client/src/About.js
@@ -1,6 +1,16 @@
 
 function About() {
-    return <h1>About</h1>;
+    return (
+        <>
+            <h1>About</h1>
+            <p>
+                ConnecT allows two people to find each other after an inital
+                meeting even if they never exchanged information about
+                themselves.
+            </p>
+            <button>An Example</button>
+        </>
+    );
 }
 
 export default About;

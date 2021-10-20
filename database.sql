5056225e984cda84c124cb61b927cae9ee2d19fa
Anderson <laventurepsu1@gmail.com>
2021-10-19 21:23:40 -0400

    refactor: updated tables

--- database.sql
+++ database.sql
@@ -7,50 +7,27 @@ CREATE TABLE users (
     user_name varchar(255) UNIQUE,
     img varchar(255),
     password varchar(255),
-    bio varchar(1000)
-);
-
-CREATE TABLE addresses (
-    address_id serial PRIMARY KEY,
-    latitude INT,
-    longitude INT
-);
-
-CREATE TABLE  demographics (
-    demographic_id serial PRIMARY KEY,
+    bio varchar(1000),
     sex varchar,
-    race varchar
-);
-
-CREATE TABLE times (
-    time_id serial PRIMARY KEY,
-    time time,
-    date date
-);
-
-CREATE TABLE heights (
-    height_id serial PRIMARY KEY,
+    race varchar,
     inches INT
+
 );
+
 
 CREATE TABLE searches (
     searches_id serial PRIMARY KEY,
-    user_id INT NOT NULL,
-    address_id INT NOT NULL,
-    time_id INT NOT NULL,
-    height_id INT NOT NULL,
+    user_id INT,
+    encountered_time time,
+    encountered_date date,
+    encountered_latitude INT,
+    encountered_longitude INT,
+    encountered_persons_race varchar,
+    encountered_persons_sex varchar,
+    encountered_persons_height INT,
+    match_found boolean,
+    match_accepted boolean,
 
     FOREIGN KEY(user_id)
-        REFERENCES users (user_id),
-
-    FOREIGN KEY(address_id)
-        REFERENCES addresses (address_id),
-
-    FOREIGN KEY(time_id)
-        REFERENCES times (time_id),
-
-    FOREIGN KEY(height_id)
-        REFERENCES heights (height_id)
+        REFERENCES users (user_id)
 );
-
-

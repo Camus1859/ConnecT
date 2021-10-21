5daddc7192a67849641eabe5318ca470a4ffa1c2
Anderson <laventurepsu1@gmail.com>
2021-10-20 20:15:00 -0400

    refactor: mySearch component

---
+++ client/src/MySearches.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const MySearches = () => {
+    const location = useLocation();
+
+    const searches = location.state?.user.map((search, index) => (
+        <li key={index}>{search} <button>Edit</button><button>Delete</button></li>
+    ));
+    // Add the ability to click on the search to get its details and update it
+
+    return (
+        <>
+            <p>Searches</p>
+
+            <ul>{searches} </ul>
+        </>
+    );
+};
+
+export default MySearches;

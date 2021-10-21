5dbd8040d6c9af48c08e498e4d4de84d9a7dfbcb
Anderson <laventurepsu1@gmail.com>
2021-10-21 01:01:03 -0400

    refactor: added Map component

---
+++ client/src/Map.js
@@ -0,0 +1,54 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+mapboxgl.accessToken =
+    'pk.eyJ1Ijoic2Nob3BlbmhhdXIiLCJhIjoiY2t1enN1MGhqN2ZhaTJucWpiYTd5aWNlbCJ9.iO_pudJDsq2K6beViTwEXg';
+
+export default function Map() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom,
+        });
+    });
+
+    useEffect(() => {
+        console.log('ran');
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl,
+            })
+        );
+    },[]);
+
+    return (
+        <div>
+            <div className='sidebar'>
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className='map-container' />
+        </div>
+    );
+}

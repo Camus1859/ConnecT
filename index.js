f038520dc9b2ed7fe5d07c152ed726d6126c763d
Camus1859 <laventurepsu1@gmail.com>
2021-11-14 19:27:56 -0500

    updated index file

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 
-app.use(express.static("client/build/"));
+// app.use(express.static("client/build/"));
 
 
 

2d790a29331d15c5b0723e5c1230a034139def9d
Camus1859 <laventurepsu1@gmail.com>
2021-11-14 21:04:53 -0500

    updated index

--- index.js
+++ index.js
@@ -570,7 +570,7 @@ app.put('/search/user', (req, res) => {
 
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile('index.html', { root });
 })
 

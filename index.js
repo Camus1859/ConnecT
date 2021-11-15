2d696a54eac04688e0fe804d1345ba2c5ed762e4
Camus1859 <laventurepsu1@gmail.com>
2021-11-14 19:40:35 -0500

    updated index

--- index.js
+++ index.js
@@ -44,18 +44,11 @@ app.use(express.json({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(express.static(path.join(__dirname, '/client/build/')));
+app.use(express.static(path.join(__dirname, '/client/build/')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-//   });
-
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
-
-// app.use(express.static("client/build/"));
-
 
 
 

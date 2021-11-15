ca1ecf1c92640e0012d986b6596c010feff0b79d
Camus1859 <laventurepsu1@gmail.com>
2021-11-14 20:27:00 -0500

    updated index

--- index.js
+++ index.js
@@ -561,11 +561,11 @@ app.put('/search/user', (req, res) => {
 });
 
 
-// app.use(express.static(path.join(__dirname, '/client/build/')));
+app.use(express.static(path.join(__dirname, '/client/build/')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-//   });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
 
 
 app.listen(PORT, () => {

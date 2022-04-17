4b55ec3d4216f7f53bda37886bfaa13662bc7efe
Anderson Laventure <Laventurepsu1@gmail.com>
2022-04-17 12:37:54 -0400

    refactor: fixes SSL server error

--- src/db/config.js
+++ src/db/config.js
@@ -2,13 +2,25 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+let pool;
+
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}`;
+if (isProduction) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+} else {
+// if on local
+    pool = new Pool({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      database: process.env.POSTGRES_DB,
+    });
+}
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: { rejectUnauthorized: false },
-});
 
 module.exports = { pool };

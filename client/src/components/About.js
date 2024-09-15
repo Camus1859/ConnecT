2eae2c98e2c57e7b81cee87f974aeb0acaa5d1bd
Anderson Laventure <laventurepsu1@gmail.com>
2024-09-15 07:56:12 -0400

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,8 +3,7 @@ function About() {
     <>
       <h1>About</h1>
       <p>
-        ConnecT allows two people to find each other after an inital meeting
-        even if they never exchanged information about themselves.
+
       </p>
       <button>An Example</button>
     </>
